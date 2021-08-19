29ea2b2daa9cb705de34595d3739bb816f32f031
mhdhfz <mhd.hfzz@gmail.com>
2021-08-19 14:09:48 +0800

    Add project files.

---
+++ JustLeave/Controllers/LeaveController.cs
@@ -0,0 +1,38 @@
+﻿using JustLeave.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustLeave.Controllers
+{
+    public class LeaveController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Leave> leaves = new List<Leave>()
+            {
+                new Leave
+                {
+                    Id = 1,
+                    LeaveType = "Annual",
+                    StartDateLeave = new DateTime(2021, 08, 19),
+                    EndDateLeave = new DateTime(2021, 08, 19),
+                    Day = 1,
+                    Reason = "Personal"
+                },
+                new Leave
+                {
+                    Id = 2,
+                    LeaveType = "Replacement",
+                    StartDateLeave = new DateTime(2021, 08, 21),
+                    EndDateLeave = new DateTime(2021, 08, 21),
+                    Day = 1,
+                    Reason = "going to register class for my kids"
+                }
+            };
+            return View(leaves);
+        }
+    }
+}
---
+++ JustLeave/Models/Leave.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustLeave.Models
+{
+    public class Leave
+    {
+        public int Id { get; set; }
+        public string LeaveType { get; set; }
+        public DateTime StartDateLeave { get; set; }
+        public DateTime EndDateLeave { get; set; }
+        public int Day { get; set; }
+        public string Reason { get; set; }
+
+
+    }
+}
